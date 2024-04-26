0cf73b018da8536622cfa59da698b0a7ce07b8b3
Jeffreylwang94 <jefflwang94@gmail.com>
2024-04-26 15:02:45 -0700

    Test VSCOde commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(input_string,repetitions) {
+    let output ="";
+    if (repetitions < 0) return "ERROR";
+    for (let i = 1; i<=repetitions; i++){
+        output = output + input_string;
+    } 
+    return output;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input_string) {
+    let output_string = "";
+
+    for(let i = 0; i < input_string.length; i++){
+        output_string =output_string+ (input_string[input_string.length-1-i]);
+    }
+    return(output_string);
+
 
 };
 
